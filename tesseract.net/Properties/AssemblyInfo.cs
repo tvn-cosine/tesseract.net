3b37a87f0b9ee765fefa055f98d4aa6bc8ea667e
tvn-cosine <tvn-cosine>
2018-01-24 07:38:26 +0200

    changed nuspec version

--- tesseract.net/Properties/AssemblyInfo.cs
+++ tesseract.net/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("tesseract.net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".net wrapper for Tesseract")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("tvn-cosine")]
 [assembly: AssemblyProduct("tesseract.net")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © tvn-cosine 2018")]
+[assembly: AssemblyTrademark("tvn-cosine 2017")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.10")]
+[assembly: AssemblyFileVersion("4.0.0.10")]
