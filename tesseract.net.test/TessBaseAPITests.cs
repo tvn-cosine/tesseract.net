b5529df133f64435afa75e643333602c9c07c0eb
tvn-cosine <tvn-cosine>
2018-01-24 13:25:03 +0200

    added unit test

---
+++ tesseract.net.test/TessBaseAPITests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tesseract;
+
+namespace tesseract.net.test
+{
+    [TestClass()]
+    public class TessBaseAPITests
+    {
+        [TestMethod()]
+        public void GetVersionTest()
+        {
+            TessBaseAPI tessBaseAPI = new TessBaseAPI();
+
+            Assert.AreEqual("4.00.00alpha", tessBaseAPI.GetVersion());
+        }
+    }
+}
