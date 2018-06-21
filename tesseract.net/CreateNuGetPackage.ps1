dac7ee86746ef6da38919e80085bcd810f5d1868
tvn-cosine <tvn-cosine>
2018-06-21 12:59:41 +0200

    added multiple targets

--- tesseract.net/CreateNuGetPackage.ps1
+++ tesseract.net/CreateNuGetPackage.ps1
@@ -1,4 +1,25 @@
-﻿Write-Host "########" 
+﻿Function Find-MsBuild([int] $MaxVersion = 2017)
+{
+    $agentPath = "$Env:programfiles (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe"
+    $devPath = "$Env:programfiles (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe"
+    $proPath = "$Env:programfiles (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe"
+    $communityPath = "$Env:programfiles (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+    $fallback2015Path = "${Env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe"
+    $fallback2013Path = "${Env:ProgramFiles(x86)}\MSBuild\12.0\Bin\MSBuild.exe"
+    $fallbackPath = "C:\Windows\Microsoft.NET\Framework\v4.0.30319"
+		
+    If ((2017 -le $MaxVersion) -And (Test-Path $agentPath)) { return $agentPath } 
+    If ((2017 -le $MaxVersion) -And (Test-Path $devPath)) { return $devPath } 
+    If ((2017 -le $MaxVersion) -And (Test-Path $proPath)) { return $proPath } 
+    If ((2017 -le $MaxVersion) -And (Test-Path $communityPath)) { return $communityPath } 
+    If ((2015 -le $MaxVersion) -And (Test-Path $fallback2015Path)) { return $fallback2015Path } 
+    If ((2013 -le $MaxVersion) -And (Test-Path $fallback2013Path)) { return $fallback2013Path } 
+    If (Test-Path $fallbackPath) { return $fallbackPath } 
+        
+    throw "Yikes - Unable to find msbuild"
+}
+
+Write-Host "########" 
 
 $projectName = "tesseract.net"
 $GitExe = "C:\Program Files\Git\bin\git.exe"
@@ -11,6 +32,7 @@ $GitHttpsUrl = "https://github.com/tvn-cosine/tesseract.net"
 Write-Host "########"
 Write-Host "Creating directory $localDirectory ..."
 Write-Host "########"
+$MSBuild = Find-MsBuild
 
 if (Test-Path "$localDirectory\\$projectName\\")
 {
@@ -73,7 +95,12 @@ Write-Host " "
 Write-Host "########"
 Write-Host "Packing nuget into $localDirectory." 
 Write-Host "Packing $localDirectory\$projectName\$projectName.csproj"
-& "C:\csource\nuget.exe" restore "$localDirectory$projectName.sln"
+& "C:\csource\nuget.exe" restore "$localDirectory$projectName\$projectName.csproj"
+
+& $MSBuild "$localDirectory$projectName\$projectName.csproj" /p:Configuration=net452;
+& $MSBuild "$localDirectory$projectName\$projectName.csproj" /p:Configuration=net462;
+& $MSBuild "$localDirectory$projectName\$projectName.csproj" /p:Configuration=net471;
+
 & "C:\csource\nuget.exe" pack "$localDirectory\$projectName\$projectName.csproj" -build
 Write-Host "Package created..." 
 Write-Host "########"
